57b1e408fb0982387fb1217b205f088d086430d9
fread007 <siansigsavourat@gmail.com>
2025-04-09 10:22:55 +0200

    constructeur class DataFrame avec un tableau

--- src/main/java/DevOps/DataFrame.java
+++ src/main/java/DevOps/DataFrame.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class DataFrame {
     ArrayList<Colonne<?>> colonnes;
 
+    public DataFrame(ArrayList<String> label, ArrayList<ArrayList<?>> entree) {
+        ArrayList<Colonne<?>> colonnes = new ArrayList<>();
+        int i = 0;
+        for (ArrayList<?> col : entree) {
+            Colonne<?> colonne = new Colonne<>(label.get(i), col);
+            colonnes.add(colonne);
+            i++;
+        }
+        this.colonnes = colonnes;
+    }
+
 }
