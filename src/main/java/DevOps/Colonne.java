064326f1ec65b1d77a9ebbe23d25ef5564f22bd8
fread007 <siansigsavourat@gmail.com>
2025-04-09 08:39:17 +0200

    constructeur class Colonne

--- src/main/java/DevOps/Colonne.java
+++ src/main/java/DevOps/Colonne.java
@@ -5,4 +5,10 @@ import java.util.ArrayList;
 public class Colonne<T> {
     String labels;
     ArrayList<T> values;
+
+    public Colonne(String labels, ArrayList<T> values) {
+        this.labels = labels;
+        this.values = values;
+    }
+    
 }
