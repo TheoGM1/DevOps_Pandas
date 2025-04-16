7c2c4f89c6858631d59d5df9982c335e17c7f51d
thehenr <henr7th@gmail.com>
2025-04-16 10:02:35 +0200

    DataFrame print implementation

--- src/main/java/DevOps/DataFrame.java
+++ src/main/java/DevOps/DataFrame.java
@@ -19,4 +19,59 @@ public class DataFrame {
         return series;
     }
 
+    public String printLabels(){
+        StringBuilder lineToPrint ;
+        for (Series s : series) {
+            lineToPrint.append(s.getLabel());
+        }
+        return lineToPrint.toString() ;
+    }
+
+    public String printLine(int numLine){
+        StringBuilder lineToPrint ;
+        for (Series s : series) {
+            lineToPrint.append(s.getValue(numLine).toString());
+        }
+        return lineToPrint.toString() ;
+    }
+
+    public String printFirstLines(int nbrLinesToPrint){
+        int nbrLines = nbrLinesToPrint ;
+        if(nbrLinesToPrint > series.size()){
+            nbrLines = series.size();
+        }
+        StringBuilder linesToPrint ;
+        linesToPrint.append(printLine(0));
+        for(int i=1 ; i<nbrLines ; i++){
+            linesToPrint.append("\n");
+            linesToPrint.append(printLine(i));
+        }
+        return linesToPrint.toString();
+    }
+
+    public String printLastLines(int nbrLinesToPrint){
+        int nbrLines = nbrLinesToPrint ;
+        if(nbrLinesToPrint > series.size()){
+            nbrLines = series.size();
+        }
+        int firstIndice = series.size() - nbrLines ;
+        StringBuilder linesToPrint ;
+        linesToPrint.append(printLine(firstIndice));
+        for(int i=firstIndice+1 ; i<nbrLines ; i++){
+            linesToPrint.append("\n");
+            linesToPrint.append(printLine(i));
+        }
+        return linesToPrint.toString();
+    }
+
+    public String printAll(){
+        StringBuilder linesToPrint ;
+        linesToPrint.append(printLabels());
+        for(int i=0 ; i<series.size() ; i++){
+            linesToPrint.append("\n");
+            linesToPrint.append(printLine(i));
+        }
+        return linesToPrint.toString() ;
+    }
+
 }
---
+++ src/test/java/DevOps/DataFramePrintTest.java
@@ -0,0 +1,14 @@
+package DevOps;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class DataFramePrintTest {
+        @Test
+        public void shouldAnswerWithTrue() {
+            assertTrue(true);
+        }
+}
